678ba6fb07f0a6e718de7322de89f85e1a6423b1
Louw Swart <louw@ouq77.kiwi>
2022-10-02 00:50:13 +1300

    Switch to cloudinary (#6)
    * Switch to cloudinary
    
    * Add missing peer dependency

--- next.config.js
+++ next.config.js
@@ -4,22 +4,17 @@ const nextConfig = {
     minimumCacheTTL: 31536000,
     remotePatterns: [
       {
-        hostname: process.env.HOSTNAME,
-        pathname: '/api/ig/*',
-        ...(process.env.PORT && { port: process.env.PORT }),
-        protocol: process.env.PROTOCOL,
+        hostname: process.env.IMAGE_HOST_NAME,
+        pathname: `/${process.env.IMAGE_HOST_PATH}/*`,
+        protocol: process.env.IMAGE_HOST_PROTOCOL,
       },
     ],
   },
   publicRuntimeConfig: {
-    // Will be available on both server and client
     igImgIds: process.env.ISTAGRAM_IMAGE_IDS,
+    imgHost: process.env.IMAGE_HOST,
   },
   reactStrictMode: true,
-  serverRuntimeConfig: {
-    host: process.env.HOST,
-    imageHost: process.env.IMAGE_HOST,
-  },
   swcMinify: true,
 };
 
