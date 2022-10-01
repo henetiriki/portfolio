9ba9bdb3a9ddba8353650fd46baf6b6629a8fabf
Louw Swart <louw@ouq77.kiwi>
2022-10-01 16:37:31 +1300

    Update remote patterns

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   images: {
     minimumCacheTTL: 31536000,
+    remotePatterns: [
+      {
+        hostname: process.env.IMAGE_HOST_NAME,
+        pathname: `${process.env.IMAGE_HOST_PATH}/*`,
+        protocol: process.env.IMAGE_HOST_PROTOCOL,
+      },
+    ],
   },
   publicRuntimeConfig: {
     igImgIds: process.env.ISTAGRAM_IMAGE_IDS,
