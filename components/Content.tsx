dc5032b579bf9c89a166ba0108be8bcd2611c202
Louw Swart <louw@ouq77.kiwi>
2022-10-02 11:34:47 +1300

    Use next/image where possible

--- components/Content.tsx
+++ components/Content.tsx
@@ -1,17 +1,26 @@
-import { Row, styled } from '@nextui-org/react';
+import { Row } from '@nextui-org/react';
+import Image from 'next/image';
 import { FC, PropsWithChildren } from 'react';
-import { contentWrapper, waveImg, waveWrapper } from '@styles';
-
-const WaveImg = styled('img', waveImg);
+import { contentWrapper, waveWrapper } from '@styles';
 
 export const Content: FC<PropsWithChildren> = ({ children }): JSX.Element => (
   <>
     <Row css={waveWrapper}>
-      <WaveImg alt='' src='/images/waves/content-top-haikei.svg' />
+      <Image
+        alt=''
+        layout='fill'
+        objectFit='cover'
+        src='/images/waves/content-top-haikei.svg'
+      />
     </Row>
     <Row css={contentWrapper}>{children}</Row>
     <Row css={{ ...waveWrapper, mb: '8rem' }}>
-      <WaveImg alt='' src='/images/waves/content-bottom-haikei.svg' />
+      <Image
+        alt=''
+        layout='fill'
+        objectFit='cover'
+        src='/images/waves/content-bottom-haikei.svg'
+      />
     </Row>
   </>
 );
