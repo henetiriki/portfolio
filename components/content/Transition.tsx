7d4fc370ca7f1469b6c3aee68dc2c57e3ae75309
Louw Swart <louw@ouq77.kiwi>
2022-10-10 19:33:06 +1300

    Add loading state while transitioning.

---
+++ components/content/Transition.tsx
@@ -0,0 +1,24 @@
+import { Container, Loading } from '@nextui-org/react';
+import { FC } from 'react';
+
+export const Transition: FC = (): JSX.Element => (
+  <Container
+    css={{
+      bc: 'rgba(12, 14, 39, 0.80)',
+      minHeight: '100vh',
+      minWidth: '100vw',
+      ov: 'hidden',
+      position: 'fixed',
+      zIndex: 200,
+    }}>
+    <Loading
+      color='primary'
+      css={{
+        left: '48vw',
+        position: 'absolute',
+        top: '50vh',
+      }}
+      type='points'
+    />
+  </Container>
+);
--- components/content/index.ts
+++ components/content/index.ts
@@ -1,3 +1,4 @@
 export * from './Content';
 export * from './FsBackground';
 export * from './Header';
+export * from './Transition';
