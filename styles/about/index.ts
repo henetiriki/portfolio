43c90e07b312daa5a445edbcd6d8ce81f694b8be
Louw Swart <louw@ouq77.kiwi>
2022-10-10 19:31:12 +1300

    Move about css into styles/about

---
+++ styles/about/index.ts
@@ -0,0 +1,28 @@
+import { CSS } from '@nextui-org/react';
+
+export const aboutContainer: CSS = {
+  d: 'flex',
+  fd: 'column',
+  fw: 'nowrap',
+  /* eslint-disable sort-keys/sort-keys-fix */
+  '@xs': {
+    fd: 'row',
+  },
+  /* eslint-enable sort-keys/sort-keys-fix */
+};
+
+export const imageContainer: CSS = {
+  '& img': {
+    borderColor: '$whisper !important',
+    borderStyle: 'solid  !important',
+    br: '$xs',
+    bw: '$xl  !important',
+  },
+  mt: '$3xl',
+  mw: '400px',
+  /* eslint-disable sort-keys/sort-keys-fix */
+  '@xs': {
+    mt: 0,
+  },
+  /* eslint-enable sort-keys/sort-keys-fix */
+};
