65a07d7fa4b3d0f0e68cb71157495880f28cf82e
Louw Swart <louw@ouq77.kiwi>
2022-10-12 09:40:17 +1300

    Update manifest and add PWA

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
+/** @type {import('next').NextConfig} */
 // @ts-check
+/* eslint-disable import/order */
+const runtimeCaching = require('next-pwa/cache');
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  runtimeCaching,
+});
+/* eslint-enable import/order */
 
 const withoutBundleAnalyzer = config => config;
 
@@ -9,27 +17,28 @@ const withBundleAnalyzer =
       })
     : withoutBundleAnalyzer;
 
-/** @type {import('next').NextConfig} */
-const nextConfig = withBundleAnalyzer({
-  images: {
-    minimumCacheTTL: 31536000,
-    remotePatterns: [
-      {
-        hostname: process.env.IMAGE_HOST_NAME,
-        pathname: `/${process.env.IMAGE_HOST_PATH}/*`,
-        protocol: process.env.IMAGE_HOST_PROTOCOL,
-      },
-    ],
-  },
-  publicRuntimeConfig: {
-    googleApiKey: process.env.GOOGLE_MAPS_API_KEY,
-    imgHost: process.env.IMAGE_HOST,
-  },
-  reactStrictMode: true,
-  serverRuntimeConfig: {
-    igImgIds: process.env.ISTAGRAM_IMAGE_IDS,
-  },
-  swcMinify: true,
-});
+const nextConfig = withBundleAnalyzer(
+  withPWA({
+    images: {
+      minimumCacheTTL: 31536000,
+      remotePatterns: [
+        {
+          hostname: process.env.IMAGE_HOST_NAME,
+          pathname: `/${process.env.IMAGE_HOST_PATH}/*`,
+          protocol: process.env.IMAGE_HOST_PROTOCOL,
+        },
+      ],
+    },
+    publicRuntimeConfig: {
+      googleApiKey: process.env.GOOGLE_MAPS_API_KEY,
+      imgHost: process.env.IMAGE_HOST,
+    },
+    reactStrictMode: true,
+    serverRuntimeConfig: {
+      igImgIds: process.env.ISTAGRAM_IMAGE_IDS,
+    },
+    swcMinify: true,
+  })
+);
 
 module.exports = nextConfig;
