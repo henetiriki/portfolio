1f124e5b367a98671303dba3e7108d37cf80cece
Louw Swart <louw@ouq77.kiwi>
2022-10-12 11:26:46 +1300

    Fix missing comma in manifest

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -27,7 +27,7 @@ const Portfolio: NextPage<AppProps> = ({
         <title key='pageTitle'>{fullTitle('Portfolio')}</title>
         <meta content='width=device-width, initial-scale=1' name='viewport' />
         <meta
-          content='On-line Portfolio and CV for Louw Swart - Front-end Engineer based in Wellington, New Zealand, using Javascript frameworks such as React, Next.js and Node.js'
+          content='Online Portfolio and CV for Louw Swart - Front-end Engineer based in Wellington, New Zealand, using Javascript frameworks such as React, Next.js and Node.js'
           key='pageDescription'
           name='description'
         />
