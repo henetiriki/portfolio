0b68b9608d005d127224bc821f0a637e00144559
Louw Swart <louw@ouq77.kiwi>
2022-10-14 08:58:05 +1300

    Lazy load about image

--- pages/index.tsx
+++ pages/index.tsx
@@ -120,6 +120,7 @@ const Home: NextPage = ({
               alt='Louw Swart'
               blurDataURL={blurDataURL(350, 350)}
               height='350px'
+              lazyBoundary='0px'
               placeholder='blur'
               src='/images/about/louw.swart.jpg'
               width='350px'
