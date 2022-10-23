d744dad2fca93bcb490aaedef76ff10c773cc50a
Louw Swart <louw@ouq77.kiwi>
2022-10-23 17:39:29 +1300

    Add headers in correct config this time

--- next.config.js
+++ next.config.js
@@ -16,8 +16,44 @@ const withPWA =
       })
     : withoutPWA;
 
+const securityHeaders = [
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on',
+  },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=63072000; includeSubDomains; preload',
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block',
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'DENY',
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'strict-origin-when-cross-origin',
+  },
+];
+
 const nextConfig = withBundleAnalyzer(
   withPWA({
+    async headers() {
+      return [
+        {
+          headers: securityHeaders,
+          // Apply these headers to all routes in your application.
+          source: '/:path*',
+        },
+      ];
+    },
     images: {
       minimumCacheTTL: 31536000,
       remotePatterns: [
