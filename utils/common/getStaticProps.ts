3ace2b60b6f52c84f37e8ba27cd8802c032fe9ee
Louw Swart <louw@ouq77.kiwi>
2023-01-23 09:17:54 +1300

    Regenerate imgId for getStaticProps

--- utils/common/getStaticProps.ts
+++ utils/common/getStaticProps.ts
@@ -7,7 +7,8 @@ const { serverRuntimeConfig } = getConfig();
 const imageIds: string[] = serverRuntimeConfig.igImgIds?.split(',') || [];
 
 export const getStaticProps: GetStaticProps = async () => {
-  const data: ImageId = { imgId: randomItem(imageIds) };
+  const imgId = await randomItem(imageIds);
+  const data: ImageId = { imgId };
 
   return {
     props: {
