55ae9cacfad45fad5cdc5424277cb2338524a2d0
Louw Swart <louw@ouq77.kiwi>
2023-08-26 17:32:58 +1200

    Add title to logo link

--- src/components/shared/Logo.tsx
+++ src/components/shared/Logo.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 import type { FC, JSX } from 'react';
 
 export const Logo: FC = (): JSX.Element => (
-  <Link href={'/'}>
+  <Link href={'/'} title='Ouwl'>
     <Image alt='Ouwl' height={40} priority src='/images/ouwl.png' width={40} />
   </Link>
 );
