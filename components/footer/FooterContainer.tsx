91f8e2ff60bf2f66c29f60ab40ebe749c0698ef4
Louw Swart <louw@ouq77.kiwi>
2023-08-26 14:31:16 +1200

    Explicitly render children.

--- components/footer/FooterContainer.tsx
+++ components/footer/FooterContainer.tsx
@@ -2,10 +2,14 @@ import { Box, useMantineTheme } from '@mantine/core';
 import type { BoxProps } from '@mantine/core';
 import type { FC } from 'react';
 
-export const FooterContainer: FC<BoxProps> = props => {
+export const FooterContainer: FC<BoxProps> = ({ children, ...others }) => {
   const {
     headings: { fontFamily: footerFf },
   } = useMantineTheme();
 
-  return <Box ff={footerFf} mx='auto' p='lg' ta='center' w='100%' {...props} />;
+  return (
+    <Box ff={footerFf} mx='auto' p='lg' ta='center' w='100%' {...others}>
+      {children}
+    </Box>
+  );
 };
