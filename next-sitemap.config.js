40988cc14689a99f12e9506695ceed1a1113edf9
Louw Swart <louw@ouq77.kiwi>
2026-01-25 12:17:58 +0200

    explicitly run next-sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   changefreq: 'monthly',
+  generateIndexSitemap: false,
   generateRobotsTxt: true,
   robotsTxtOptions: {
     policies: [
