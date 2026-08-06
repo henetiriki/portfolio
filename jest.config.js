436725c48dd891b20098c87c5a12a56d940841f1
louw swart <louw@ouwl.house>
2026-08-06 20:47:30 +0200

    Add Github CI Action

--- jest.config.js
+++ jest.config.js
@@ -7,6 +7,16 @@ const createJestConfig = nextJest({
 
 /** @type {import('jest').Config} */
 const customJestConfig = {
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/__tests__/**',
+    '!src/**/*.d.ts',
+    '!src/**/index.ts',
+    '!src/pages/_app.tsx',
+    '!src/pages/_document.tsx',
+    '!src/pages/_offline.tsx',
+    '!src/utils/test/**',
+  ],
   coveragePathIgnorePatterns: ['<rootDir>/node_modules/'],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   testEnvironment: 'jest-environment-jsdom',
