1b1ba804bd3462527752dba9d0de780dece0d9f7
louw swart <louw@ouwl.house>
2026-08-06 22:05:10 +0200

    Fix img-id causing double loading of image ids

--- src/pages/api/img-id.ts
+++ src/pages/api/img-id.ts
@@ -6,9 +6,9 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 const {
   serverRuntimeConfig: { igImgIds },
 } = getConfig();
-const imageIds: string[] = igImgIds?.split(',') || [];
 
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+const handler = async (_: NextApiRequest, res: NextApiResponse) => {
+  const imageIds: string[] = igImgIds?.split(',') || [];
   const imgId = await randomItem(imageIds);
   const data: ImageId = { imgId };
 
