b448caaf2dddb6e537df5d2fd8c217913b2c64a6
Louw Swart <louw@ouwl.house>
2026-08-09 18:31:34 +0200

    Fix route loading state machine (#126)

--- src/hooks/useLoading.ts
+++ src/hooks/useLoading.ts
@@ -2,23 +2,23 @@ import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 
 export const useLoading = (): boolean => {
-  const router = useRouter();
+  const { events } = useRouter();
   const [isLoading, setIsLoading] = useState(false);
 
-  const toggleLoading = () => setIsLoading(loading => !loading);
-
   useEffect(() => {
-    router.events.on('routeChangeStart', toggleLoading);
-    router.events.on('routeChangeComplete', toggleLoading);
-    router.events.on('routeChangeError', toggleLoading);
+    const startLoading = () => setIsLoading(true);
+    const stopLoading = () => setIsLoading(false);
+
+    events.on('routeChangeStart', startLoading);
+    events.on('routeChangeComplete', stopLoading);
+    events.on('routeChangeError', stopLoading);
 
     return () => {
-      router.events.off('routeChangeStart', toggleLoading);
-      router.events.off('routeChangeComplete', toggleLoading);
-      router.events.off('routeChangeError', toggleLoading);
+      events.off('routeChangeStart', startLoading);
+      events.off('routeChangeComplete', stopLoading);
+      events.off('routeChangeError', stopLoading);
     };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [events]);
 
   return isLoading;
 };
